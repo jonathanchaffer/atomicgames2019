a28a0247d7980843f4cdfff19b231c24442b59f1
jonathanchaffer <jonathanchaffer@gmail.com>
2019-10-26 14:55:31 -0400

    updated GatherStrategy to use general direction

--- intellij/src/main/java/com/atomicobject/games/rts/strategies/GatherStrategy.java
+++ intellij/src/main/java/com/atomicobject/games/rts/strategies/GatherStrategy.java
@@ -7,13 +7,17 @@ import com.atomicobject.games.rts.state.MapDirections;
 import com.atomicobject.games.rts.state.Unit;
 import com.atomicobject.games.rts.state.UnitManager;
 
+import java.util.Random;
+
 public class GatherStrategy implements IUnitStrategy {
     private final Map map;
     private final UnitManager unitManager;
+    private final MapDirections.Direction generalDirection;
 
     public GatherStrategy(Map map, Unit unit, UnitManager unitManager) {
         this.map = map;
         this.unitManager = unitManager;
+        this.generalDirection = MapDirections.randomDirection();
     }
 
     public AICommand buildCommand(Unit unit) {
@@ -38,8 +42,15 @@ public class GatherStrategy implements IUnitStrategy {
                 return AICommand.buildMoveCommand(unit, direction);
             }
         }
+        // go in the general direction according to odds
         var direction = MapDirections.randomDirection();
+        var odds = new int[]{1,1,1,1,2,2,2,3,3,3,4,4};
+        var random = new Random();
+        var num = odds[random.nextInt(odds.length)];
+        if (num == 1) direction = generalDirection; // turn 0
+        else if (num == 2) direction = MapDirections.turn(generalDirection); // turn 1
+        else if (num == 3) direction = MapDirections.turn(MapDirections.turn(MapDirections.turn(generalDirection))); // turn 3
+        else direction = MapDirections.turn(MapDirections.turn(generalDirection)); // turn 2
         return AICommand.buildMoveCommand(unit, direction);
     }
-
 }
