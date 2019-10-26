e04d41c9c07d1e11d2c8febf4eee535cb9dcaed5
jonathanchaffer <jonathanchaffer@gmail.com>
2019-10-26 10:48:19 -0400

    removed eclipse dir

--- eclipse/src/main/java/com/atomicobject/games/rts/strategies/IUnitStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.atomicobject.games.rts.strategies;
-
-import com.atomicobject.games.rts.communication.AICommand;
-import com.atomicobject.games.rts.state.Unit;
-
-public interface IUnitStrategy {
-    AICommand buildCommand(Unit unit);
-}
